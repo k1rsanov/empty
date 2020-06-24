5951a409dbe94e64c96034ebcc6499da41e23557
kirsanov_stanislav <stanislau_kirsanau@epam.com>
2020-06-24 15:30:02 +0300

    initial

---
+++ selectors.js
@@ -0,0 +1,4 @@
+export const getWordsSelector = (store) => store.dictionary.words;
+export const getWorldCountSelector = (store) => store.dictionary.worldCount;
+export const getWorldCountTodaySelector = (store) => store.dictionary.worldCountToday;
+export const getLosingFlagSelector = (store) => store.weather.isLoading;
