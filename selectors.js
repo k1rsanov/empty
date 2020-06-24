1dc36ac50e005daf5c37ddfbb95be77258a3a9a9
kirsanov_stanislav <stanislau_kirsanau@epam.com>
2020-06-24 16:00:13 +0300

    init

--- selectors.js
+++ selectors.js
@@ -1,15 +1,1 @@
-xxx master
-
-export const getWordsSelector = (store) => store.dictionary.words;
-
-export const getWorldCountSelector = (store) => store.dictionary.worldCount;
-
-export const getWorldCountTodaySelector = (store) => store.dictionary.worldCountToday;
-
-export const getLosingFlagSelector = (store) => store.weather.isLoadingzzz;
-
-
-export const getLosingFlagSelector = (store) => store.weather.isLoadingXx;
-export const getLosingFlagSelector = (store) => store.weather.isLoadingXx2;
-export const getLosingFlagSelector = (store) => store.weather.isLoadingMasterrr;
-export const getLosingFlagSelector = (store) => store.weather.isLoadingMasterr2;
+master
