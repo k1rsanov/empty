ad186d625d1abeffb4b19124c88e4b88287a48ce
kirsanov_stanislav <stanislau_kirsanau@epam.com>
2020-06-24 15:32:16 +0300

    test people cm

--- selectors.js
+++ selectors.js
@@ -1,4 +1,1 @@
-export const getWordsSelector = (store) => store.dictionary.words;
-export const getWorldCountSelector = (store) => store.dictionary.worldCount;
-export const getWorldCountTodaySelector = (store) => store.dictionary.worldCountToday;
-export const getLosingFlagSelector = (store) => store.weather.isLoading;
+export const getWordsSelector = (store) => store.dictionaryQwe.words;
