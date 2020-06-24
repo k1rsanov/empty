22289b6476e5939421d58e6fcaed01ff421f7154
kirsanov_stanislav <stanislau_kirsanau@epam.com>
2020-06-24 15:45:01 +0300

    add zzz

--- selectors.js
+++ selectors.js
@@ -4,7 +4,7 @@ export const getWorldCountSelector = (store) => store.dictionary.worldCount;
 
 export const getWorldCountTodaySelector = (store) => store.dictionary.worldCountToday;
 
-export const getLosingFlagSelector = (store) => store.weather.isLoading;
+export const getLosingFlagSelector = (store) => store.weather.isLoadingzzz;
 
 
 export const getLosingFlagSelector = (store) => store.weather.isLoadingXx;
