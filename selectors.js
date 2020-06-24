717782457ee5dc0815a762417d779a0219945234
kirsanov_stanislav <stanislau_kirsanau@epam.com>
2020-06-24 15:52:42 +0300

    w1

--- selectors.js
+++ selectors.js
@@ -10,3 +10,4 @@ export const getLosingFlagSelector = (store) => store.weather.isLoadingzzz;
 export const getLosingFlagSelector = (store) => store.weather.isLoadingXx;
 export const getLosingFlagSelector = (store) => store.weather.isLoadingXx2;
 export const getLosingFlagSelector = (store) => store.weather.isLoadingMasterrr;
+export const getLosingFlagSelector = (store) => store.weather.isLoadingMasterr2;
